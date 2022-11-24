b6371441751619ecee557753a71e1dd97d32b807
comconToukir <ahmdtoukir@gmail.com>
2022-11-25 03:01:15 +0600

    add product route added

--- index.js
+++ index.js
@@ -21,12 +21,45 @@ const run = async () => {
   try {
     const bazaarDb = client.db("bazaarDb");
     const usersCollection = bazaarDb.collection("users");
+    const productsCollection = bazaarDb.collection("products");
 
-    app.post('/user', async (req, res) => {
+    // if new user add if logged in with google
+    app.put('/user', async (req, res) => {
       const user = req.body;
 
+      const filter = { email: user.email };
+
+      const options = { upsert: true };
+
+      const updateDoc = {
+        $set: {
+          name: user.name,
+          email: user.email,
+          role: user.role
+        }
+      }
+
       // console.log(user);
-      const result = usersCollection.insertOne(user);
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+
+      res.send(result);
+    })
+
+    // check the user's role
+    app.get('/users/role/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email }
+
+      const user = await usersCollection.findOne(query);
+
+      res.send(user);
+    })
+
+    // create a new product
+    app.post('/add-product', async (req, res) => {
+      const productData = req.body;
+
+      const result = await productsCollection.insertOne(productData);
 
       res.send(result);
     })
