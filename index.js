e0ee72c290c59035b35e89fef7473cf3ea899851
comconToukir <ahmdtoukir@gmail.com>
2022-11-26 18:01:07 +0600

    updated put book product & get book product

--- index.js
+++ index.js
@@ -158,13 +158,176 @@ const run = async () => {
     })
 
     // book a product
-    app.post('/book-product', async (req, res) => {
+    app.put('/book-product', async (req, res) => {
       const booking = req.body;
 
-      const result = await bookingsCollection.insertOne(booking);
+      console.log(booking);
 
-      res.send(result);
+      const booker = {
+        bookerName: booking.fullName,
+        bookerEmail: booking.email,
+        bookerLocation: booking.location,
+        bookerNumber: booking.phoneNumber
+      }
+
+      const bookingData = {
+        productId: ObjectId(booking.productId),
+        image: booking.image,
+        price: booking.price,
+        productName: booking.productName,
+        sellerContact: booking.sellerContact,
+        sellerEmail: booking.sellerEmail,
+        sellerLocation: booking.sellerLocation,
+        bookers: [booker]
+
+      }
+
+      const filter = { productId: ObjectId(booking.productId) };
+
+      const updateDoc = {
+        $push: {
+          bookers: booker
+        }
+      }
+
+      const options = { upsert: true }
+
+      const result = await bookingsCollection.findOne(filter);
+
+      if (result) {
+        const updateResult = await bookingsCollection.updateOne(filter, updateDoc, options);
+
+        res.send(updateResult);
+      }
+
+      else {
+        const insertResult = await bookingsCollection.insertOne(bookingData);
+
+        res.send(insertResult);
+      }
+
+      // gives error but updates as wanted
+      // const indexCreate = await bookingsCollection.createIndex({ "productId": 1 }, { "unique": true })
+
+      // console.log(indexCreate)
+
+      // const result = await bookingsCollection.bulkWrite([
+      //   {
+      //     "updateOne": {
+      //       'filter': { 'productId': booking.productId },
+      //       'update': {
+      //         "$push": {
+      //           "bookers": {
+      //             "bookerName": booking.fullName,
+      //             'bookerEmail': booking.email,
+      //             'bookerLocation': booking.location,
+      //             "bookerNumber": booking.phoneNumber
+      //           }
+      //         }
+      //       }
+      //     }
+      //   },
+      //   {
+      //     'insertOne': {
+      //       'document': {
+      //         'productId': booking.productId,
+      //         "image": booking.image,
+      //         "productName": booking.productName,
+      //         "sellerContact": booking.sellerContact,
+      //         "sellerEmail": booking.sellerEmail,
+      //         "sellerLocation": booking.sellerLocation,
+      //         "bookers": [{
+      //           "bookerName": booking.fullName,
+      //           "bookerEmail": booking.email,
+      //           'bookerLocation': booking.location,
+      //           'bookerNumber': booking.phoneNumber
+      //         }]
+
+      //       }
+      //     }
+      //   }
+      // ],
+      // { "ordered": false }
+      // )
+
+      // console.log(result);
+
+      // res.send(result);
     })
+
+    // get bookings of a single buyer
+    app.get('/book-product', async (req, res) => {
+      const email = req.query.email;
+
+      console.log(email);
+
+      // const query = {
+      //   bookers: {
+      //     $elemMatch: { bookerEmail: email }
+      //   }
+      // };
+
+      // to get only users own data in bookers
+      const pipeline = [
+        {
+          "$match": {
+            "bookers": {
+              "$elemMatch": {
+                "$and": [
+                  { "bookerEmail": email }
+                ]
+              }
+            }
+          }
+        },
+        {
+          "$project": {
+            "productId": 1,
+            "image": 1,
+            "productName": 1,
+            "price": 1,
+            "sellerEmail": 1,
+            "sellerContact": 1,
+            "sellerName": 1,
+            "bookers": {
+              "$filter": {
+                "input": "$bookers",
+                "as": "bookers",
+                "cond": {
+                  "$and": [
+                    { "$eq": ["$$bookers.bookerEmail", email] }
+                  ]
+                }
+              }
+            }
+          }
+        }
+      ]
+
+      const options = {
+        sort: { _id: -1 }
+      }
+
+      // const products = await bookingsCollection.find(query, options).toArray();
+      const products = await bookingsCollection.aggregate(pipeline).sort({ _id: -1 }).toArray()
+
+      console.log(products);
+
+      res.send(products);
+    })
+
+    // delete a single booking
+    app.delete('/book-product', async (req, res) => {
+      const email = req.query.email;
+      const id = req.query.id;
+
+      console.log(email, id);
+
+      const query = { email: email }
+
+      res.send('req received')
+    })
+
   }
   finally {
 
